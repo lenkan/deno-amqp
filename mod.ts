251e59afaa12749f25693bbc0f7b1b1fe9b21681
lenkan <dlenksjo@gmail.com>
2020-04-11 11:36:11 +0200

    fix formatting

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-export {  connect, AmqpConnectOptions } from "./src/amqp_connect.ts";
+export { connect, AmqpConnectOptions } from "./src/amqp_connect.ts";
 export { AmqpConnection } from "./src/amqp_connection.ts";
 export { AmqpChannel, BasicDeliverHandler } from "./src/amqp_channel.ts";
 export * from "./src/amqp_types.ts";
