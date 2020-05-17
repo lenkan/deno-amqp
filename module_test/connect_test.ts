ab29646853c1b31c3671e109e5db5a044f9a11e9
lenkan <dlenksjo@gmail.com>
2020-05-17 22:36:35 +0200

    feature: support connection string parameter

---
+++ module_test/connect_test.ts
@@ -0,0 +1,57 @@
+import { connect } from "../mod.ts";
+import { assertThrowsAsync } from "https://deno.land/std@v0.51.0/testing/asserts.ts";
+
+Deno.test(
+  "connect url",
+  async () => {
+    const conn = await connect("amqp://guest:guest@localhost:5672");
+    await conn.close();
+  },
+);
+
+Deno.test(
+  "connect url - missing port",
+  async () => {
+    const conn = await connect("amqp://guest:guest@localhost");
+    await conn.close();
+  },
+);
+
+Deno.test(
+  "connect url - invalid username/password",
+  async () => {
+    await assertThrowsAsync(
+      async () => {
+        await connect("amqp://invaliduser:somepass@localhost:5672");
+      },
+      undefined,
+      "EOF",
+    );
+    await Promise.resolve();
+  },
+);
+
+Deno.test(
+  "connect url - wrong port",
+  async () => {
+    await assertThrowsAsync(
+      async () => {
+        await connect("amqp://guest:guest@localhost:5673");
+      },
+      Deno.errors.ConnectionRefused,
+    );
+  },
+);
+
+Deno.test(
+  "connect url - does not support amqps",
+  async () => {
+    await assertThrowsAsync(
+      async () => {
+        await connect("amqps://guest:guest@localhost:5672");
+      },
+      Error,
+      "Unsupported protocol",
+    );
+  },
+);
--- src/amqp_connect.ts
+++ src/amqp_connect.ts
@@ -1,76 +1,27 @@
 import { AmqpConnection } from "./amqp_connection.ts";
 import { AmqpSocket } from "./amqp_socket.ts";
+import { AmqpConnectOptions, parseOptions } from "./amqp_connect_options.ts";
 
-/**
- * Options for the connection to an AMQP broker.
- */
-export interface AmqpConnectOptions {
-  /**
-   * Hostname or literal IP-address to the AMQP broker. Defaults to 'localhost'.
-   */
-  hostname?: string;
+export { AmqpConnectOptions };
 
-  /**
-   * TCP port of the AMQP broker. Defaults to 5672.
-   */
-  port?: number;
-
-  /**
-   * Username for authenticating towards the AMQP broker. Defaults to 'guest'.
-   */
-  username?: string;
-
-  /**
-   * Password for authenticating towards the AMQP broker. Defaults to 'guest'.
-   */
-  password?: string;
-
-  /**
-   * AMQP virtual host. Defaults to "/"
-   */
-  vhost?: string;
-
-  /**
-   * Interval in seconds for the AMQP heartbeat frames. If not provided, the suggested heartbeat interval from
-   * the AMQP broker will be used (usually 60s).
-   *
-   * If explicitly set to 0, heartbeat frames will be disabled.
-   */
-  heartbeatInterval?: number;
-
-  /**
-   * Sets the maximum frame size in number of bytes.
-   * 
-   * This is negotiated with the broker during the connection handshake.
-   */
-  frameMax?: number;
-
-  /**
-   * **UNSTABLE**
-   * Controls the log level. Currently setting it to 'debug' will print received and sent frames on byte level.
-   *
-   * This should eventually be able to turn logging on and off on different levels such as framing/methods/connection.
-   */
-  loglevel?: "debug" | "none";
-}
-
-/**
- * Connects to an AMQP broker specified by the options.
- * @param options Connect options
- */
+export async function connect(): Promise<AmqpConnection>;
 export async function connect(
-  options: AmqpConnectOptions = {},
+  options: AmqpConnectOptions,
+): Promise<AmqpConnection>;
+export async function connect(uri: string): Promise<AmqpConnection>;
+export async function connect(
+  optionsOrUrl?: AmqpConnectOptions | string,
 ): Promise<AmqpConnection> {
   const {
-    hostname = "localhost",
-    port = 5672,
-    username = "guest",
-    password = "guest",
+    hostname,
+    port,
+    username,
+    password,
     heartbeatInterval,
-    loglevel = "none",
-    vhost = "/",
+    loglevel,
+    vhost,
     frameMax,
-  } = options;
+  } = parseOptions(optionsOrUrl);
 
   const conn = await Deno.connect({ port, hostname });
   const socket = new AmqpSocket(conn);
