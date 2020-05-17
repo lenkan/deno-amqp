f710d7db9e806dc22aadc449d9c57d7e080433e6
lenkan <dlenksjo@gmail.com>
2020-05-17 22:36:35 +0200

    feature: support connection string parameter

---
+++ src/amqp_connect_options.ts
@@ -0,0 +1,124 @@
+import { AmqpConnection } from "./amqp_connection.ts";
+import { AmqpSocket } from "./amqp_socket.ts";
+
+/**
+ * Options for the connection to an AMQP broker.
+ */
+export interface AmqpConnectOptions {
+  /**
+   * Hostname or literal IP-address to the AMQP broker. Defaults to 'localhost'.
+   */
+  hostname?: string;
+
+  /**
+   * TCP port of the AMQP broker. Defaults to 5672.
+   */
+  port?: number;
+
+  /**
+   * Username for authenticating towards the AMQP broker. Defaults to 'guest'.
+   */
+  username?: string;
+
+  /**
+   * Password for authenticating towards the AMQP broker. Defaults to 'guest'.
+   */
+  password?: string;
+
+  /**
+   * AMQP virtual host. Defaults to "/"
+   */
+  vhost?: string;
+
+  /**
+   * Interval in seconds for the AMQP heartbeat frames. If not provided, the suggested heartbeat interval from
+   * the AMQP broker will be used (usually 60s).
+   *
+   * If explicitly set to 0, heartbeat frames will be disabled.
+   */
+  heartbeatInterval?: number;
+
+  /**
+   * Sets the maximum frame size in number of bytes.
+   * 
+   * This is negotiated with the broker during the connection handshake.
+   */
+  frameMax?: number;
+
+  /**
+   * **UNSTABLE**
+   * Controls the log level. Currently setting it to 'debug' will print received and sent frames on byte level.
+   *
+   * This should eventually be able to turn logging on and off on different levels such as framing/methods/connection.
+   */
+  loglevel?: "debug" | "none";
+}
+
+export interface AmqpConnectParameters {
+  hostname: string;
+  port: number;
+  username: string;
+  password: string;
+  vhost: string;
+  heartbeatInterval?: number;
+  frameMax?: number;
+  loglevel: "debug" | "none";
+}
+
+function parseUrl(value: string): AmqpConnectOptions {
+  if (!value.startsWith("amqp:")) {
+    throw new Error("Unsupported protocol");
+  }
+
+  const url = new URL(value.replace("amqp:", "http:"));
+
+  const heartbeatParam = url.searchParams.get("heartbeat");
+  const heartbeat = heartbeatParam ? parseInt(heartbeatParam) : undefined;
+  if (heartbeat !== undefined && isNaN(heartbeat)) {
+    throw new Error(`Invalid heartbeat parameter ${heartbeatParam}`);
+  }
+
+  const frameMaxParam = url.searchParams.get("frame_max");
+  const frameMax = frameMaxParam ? parseInt(frameMaxParam) : undefined;
+  if (frameMax !== undefined && isNaN(frameMax)) {
+    throw new Error(`Invalid frame_max parameter ${frameMaxParam}`);
+  }
+
+  return {
+    hostname: url.hostname,
+    port: parseInt(url.port || "5672"),
+    username: url.username || "guest",
+    password: url.password || "guest",
+    vhost: url.pathname,
+    heartbeatInterval: heartbeat,
+    frameMax: frameMax,
+  };
+}
+
+export function parseOptions(
+  optionsOrString: string | AmqpConnectOptions = {},
+): AmqpConnectParameters {
+  const {
+    hostname = "localhost",
+    port = 5672,
+    username = "guest",
+    password = "guest",
+    heartbeatInterval,
+    loglevel = "none",
+    vhost = "/",
+    frameMax,
+  } = typeof optionsOrString === "string"
+    ? parseUrl(optionsOrString)
+    : optionsOrString;
+
+  return {
+    hostname,
+    port,
+    username,
+    password,
+    heartbeatInterval,
+    loglevel,
+    vhost,
+    frameMax,
+  };
+}
---
+++ src/amqp_connect_options_test.ts
@@ -0,0 +1,137 @@
+import {
+  parseOptions,
+  AmqpConnectParameters,
+  AmqpConnectOptions,
+} from "./amqp_connect_options.ts";
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std@v0.51.0/testing/asserts.ts";
+
+function testUrl(
+  url: string,
+  expected: AmqpConnectParameters,
+): [string, () => void] {
+  return [
+    `parse ${url}`,
+    () => {
+      assertEquals(parseOptions(url), expected);
+    },
+  ];
+}
+
+function testOpts(
+  opts: AmqpConnectOptions | undefined,
+  expected: AmqpConnectParameters,
+): [string, () => void] {
+  return [
+    `parse ${JSON.stringify(opts)}`,
+    () => {
+      assertEquals(parseOptions(opts), expected);
+    },
+  ];
+}
+
+function testUrlError(
+  url: string,
+  message?: string,
+): [string, () => void] {
+  return [
+    `parse ${url} - throws ${message}`,
+    () => {
+      assertThrows(() => parseOptions(url), undefined, message);
+    },
+  ];
+}
+
+const defaultParams: AmqpConnectParameters = {
+  hostname: "localhost",
+  port: 5672,
+  username: "guest",
+  password: "guest",
+  heartbeatInterval: undefined,
+  loglevel: "none",
+  frameMax: undefined,
+  vhost: "/",
+};
+
+Deno.test(...testUrl("amqp://user:pass@localhost:5672", {
+  ...defaultParams,
+  username: "user",
+  password: "pass",
+}));
+
+Deno.test(...testUrl("amqp://localhost", {
+  ...defaultParams,
+}));
+
+Deno.test(...testUrl("amqp://somehost.com:123", {
+  ...defaultParams,
+  hostname: "somehost.com",
+  port: 123,
+}));
+
+Deno.test(...testUrl("amqp://user:pass@somehost.com:123", {
+  ...defaultParams,
+  hostname: "somehost.com",
+  port: 123,
+  username: "user",
+  password: "pass",
+}));
+
+Deno.test(...testUrl("amqp://localhost/somevhost", {
+  ...defaultParams,
+  vhost: "/somevhost",
+}));
+
+Deno.test(...testUrl("amqp://localhost:123/somevhost", {
+  ...defaultParams,
+  vhost: "/somevhost",
+  port: 123,
+}));
+
+Deno.test(
+  ...testUrlError("amqps://localhost:123/somevhost", "Unsupported protocol"),
+);
+
+Deno.test(...testUrl("amqp://localhost?heartbeat=10", {
+  ...defaultParams,
+  heartbeatInterval: 10,
+}));
+
+Deno.test(
+  ...testUrlError(
+    "amqp://localhost?heartbeat=abc",
+    "Invalid heartbeat parameter abc",
+  ),
+);
+
+Deno.test(...testUrl("amqp://localhost?frame_max=10", {
+  ...defaultParams,
+  frameMax: 10,
+}));
+
+Deno.test(
+  ...testUrlError(
+    "amqp://localhost?frame_max=abc",
+    "Invalid frame_max parameter abc",
+  ),
+);
+
+Deno.test(...testOpts(undefined, {
+  ...defaultParams,
+}));
+
+Deno.test(...testOpts({}, {
+  ...defaultParams,
+}));
+
+Deno.test(...testOpts({ hostname: "somehost.com" }, {
+  ...defaultParams,
+  hostname: "somehost.com",
+}));
+
+Deno.test(...testOpts({ port: 123 }, {
+  ...defaultParams,
+  port: 123,
+}));
