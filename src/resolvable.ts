58106a32601028faf2bf3c1d0b4a700b010984b9
Edward Deards <edward@deards.org>
2020-12-28 21:56:18 +0000

    Fix such that resolveFun will not be given an undefined value

--- src/resolvable.ts
+++ src/resolvable.ts
@@ -6,7 +6,7 @@ export interface Resolvable<T> {
 export interface ResolvablePromise<T> extends PromiseLike<T>, Resolvable<T> {}
 
 export function createResolvable<T>(): ResolvablePromise<T> {
-  let resolveFun: (value?: T | PromiseLike<T> | Promise<T>) => void;
+  let resolveFun: (value: T | PromiseLike<T> | Promise<T>) => void;
   let rejectFun: (reason?: any) => void;
 
   const promise = new Promise<T>((resolve, reject): void => {
