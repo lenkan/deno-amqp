87fbadf16000fdd262469fc4b83f32e06cd73381
lenkan <dlenksjo@gmail.com>
2022-11-17 19:28:52 +0100

    upgrade deno 1.28.1

--- src/error_handling.ts
+++ src/error_handling.ts
@@ -3,12 +3,16 @@ import type { ChannelClose, ConnectionClose } from "./amqp_types.ts";
 
 export function serializeChannelError(channel: number, args: ChannelClose) {
   const causedBy = getMethodName(args.classId, args.methodId);
-  return `Channel ${channel} closed by server - ${args.replyCode} ${args
-    .replyText || ""}${causedBy ? ` - caused by '${causedBy}'` : ""}`;
+  return `Channel ${channel} closed by server - ${args.replyCode} ${
+    args
+      .replyText || ""
+  }${causedBy ? ` - caused by '${causedBy}'` : ""}`;
 }
 
 export function serializeConnectionError(args: ConnectionClose) {
   const causedBy = getMethodName(args.classId, args.methodId);
-  return `Connection closed by server - ${args.replyCode} ${args
-    .replyText || ""}${causedBy ? ` - caused by '${causedBy}'` : ""}`;
+  return `Connection closed by server - ${args.replyCode} ${
+    args
+      .replyText || ""
+  }${causedBy ? ` - caused by '${causedBy}'` : ""}`;
 }
